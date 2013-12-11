400bc1f60d2c4dfb0a8591ac714ca9e9c1e7b1a6
chunshin (cygwin) <l7263626@msn.com>
2013-12-11 07:50:43 +0000

    刪除不必要的libs

--- libs/libs-gzip-js.php
+++
@@ -1,8 +0,0 @@
-<?php
-ob_start ("ob_gzhandler");
-header ("content-type: text/javascript; charset: UTF-8");
-header ("cache-control: must-revalidate");
-$offset = 600 * 600;
-$expire = "expires: " . gmdate ("D, d M Y H:i:s", time() + $offset) . " GMT";
-header ($expire);
-?>
