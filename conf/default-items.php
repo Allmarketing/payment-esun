4ea570814c12ebd45902d8240f3b8bc11e6c74b2
chunhsin(toshiba) <l7263626@msn.com>
2014-01-06 11:28:49 +0000

    加入語系檔及預設項目

---
+++ conf/default-items.php
@@ -0,0 +1,12 @@
+<?php
+$ws_array["order_status"]=array(
+    0  => $TPLMSG["ORDER_NEW"],
+    1  => $TPLMSG["ORDER_DEALING"],
+    2  => $TPLMSG["ORDER_PRODUCTS_SEND"],
+    3  => $TPLMSG["ORDER_COMPLETED"],
+    9  => $TPLMSG["ORDER_CANCEL"],
+    10 => $TPLMSG["ORDER_REJECT"],
+    20 => $TPLMSG['AUTHORIZE_TERMINATE'],
+    21 => $TPLMSG['AUTHORIZE_FAILED']
+);
+?>
---
+++ lang/cht-utf8.php
@@ -0,0 +1,16 @@
+<?php
+//ORDER
+$TPLMSG["ORDER_ID"]="訂單編號";
+$TPLMSG["ORDER_TOTAL_MONEY"]="訂單金額";
+$TPLMSG["ORDER_NEW"]="新訂單";
+$TPLMSG["ORDER_DEALING"]="處理中";
+$TPLMSG["ORDER_PRODUCTS_SEND"]="出貨中";
+$TPLMSG["ORDER_COMPLETED"]="已完成";
+$TPLMSG["ORDER_CANCEL"]="<font color='red'>取消訂單</font>";
+$TPLMSG["ORDER_REJECT"]="<font color='red'>拒絕訂單</font>";
+$TPLMSG["ORDER_DETAIL"]="產品訂單詳細資料";
+$TPLMSG["ORDER_CONTENT"]="訂單內容";
+$TPLMSG['AUTHORIZE_TERMINATE'] = "授權中斷";
+$TPLMSG['AUTHORIZE_FAILED'] = "授權失敗";
+$TPLMSG['AUTHOZIED_FAILED_EXTEND_MSG'] = "<div class='auth_err_rc'>%s</div><div class='auth_err_msg'>請確認您的信用卡資訊是否正確！<br/>如果您確定輸入是正確的資訊，請與您的發卡銀行聯絡。<br/>若造成您的不便，敬請見諒。 </div>";
+?>
