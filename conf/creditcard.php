09862549d6f93f1e0705e635d045d8e7920d76ad
chunhsin(toshiba) <l7263626@msn.com>
2014-01-06 12:01:07 +0000

    清空特店代碼及mac key

--- conf/creditcard.php
+++ conf/creditcard.php
@@ -1,10 +1,10 @@
 <?php
-$cms_cfg['creditcard']['mid'] = "8089001122";//特店代碼
+$cms_cfg['creditcard']['mid'] = "";//特店代碼
 $cms_cfg['creditcard']['cid'] = "";//子特店代號，留空
 $cms_cfg['creditcard']['tid'] = "EC000001";//終端機代號，EC000001(一般交易)、EC000002(分期)
 $cms_cfg['creditcard']['ono'] = "";
 $cms_cfg['creditcard']['ta'] = "";
 $cms_cfg['creditcard']['u'] = "http://localhost/payment_esun/card-test3.php";//授權結果回傳接收頁
-$cms_cfg['esunkey'] = "EOY1NX4G9H2D1FA20TDIFVODZDDAE24O";//mac key
+$cms_cfg['esunkey'] = "";//mac key
 $cms_cfg['exe_mode'] = "testing";  //執行模式: testing or running
 ?>
