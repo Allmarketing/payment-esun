6e6e931c81309e4078218eb9d958879a64896c64
chunhsin(toshiba) <l7263626@msn.com>
2014-01-06 12:04:50 +0000

    增加ono及tu選項的說明

--- conf/creditcard.php
+++ conf/creditcard.php
@@ -2,8 +2,8 @@
 $cms_cfg['creditcard']['mid'] = "";//特店代碼
 $cms_cfg['creditcard']['cid'] = "";//子特店代號，留空
 $cms_cfg['creditcard']['tid'] = "EC000001";//終端機代號，EC000001(一般交易)、EC000002(分期)
-$cms_cfg['creditcard']['ono'] = "";
-$cms_cfg['creditcard']['ta'] = "";
+$cms_cfg['creditcard']['ono'] = "";  //訂單號碼，由model_order_payment_esun::checkout()指定
+$cms_cfg['creditcard']['ta'] = "";   //訂單價格，由model_order_payment_esun::checkout()指定
 $cms_cfg['creditcard']['u'] = "http://localhost/payment_esun/card-test3.php";//授權結果回傳接收頁
 $cms_cfg['esunkey'] = "";//mac key
 $cms_cfg['exe_mode'] = "testing";  //執行模式: testing or running
